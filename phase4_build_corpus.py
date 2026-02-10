ffc11f87cfe0b5bd6f3e238dee54b26ccb9fe31f
Context-Reviewer <refusedepositbox@gmail.com>
2026-02-09 22:06:17 -0500

    chore: relax pyright strictness for dynamic pipeline script

--- phase4_build_corpus.py
+++ phase4_build_corpus.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# pyright: reportUnknownMemberType=false
+# pyright: reportUnknownVariableType=false
+# pyright: reportGeneralTypeIssues=false
 
 import argparse
 import json
